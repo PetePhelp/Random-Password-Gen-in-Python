a8a65ea2b057775895f73d4654bc08aecbf98f9b
Tim G <34285263+PetePhelp@users.noreply.github.com>
2022-10-20 15:51:48 -0400

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,25 @@
+# Random Password Generator
+import random
+# If you have any advice on how I structure my code let me know always looking to improve B)
+
+while True:
+    try:
+        x = int(input("Choose a Length for password: "))
+        if x > 0:
+            break
+
+        elif x < 0:
+            continue
+    except ValueError:
+        print(end="")
+print("Here is your password: ")
+for _ in range(x): # Random Caps too before adding to github???
+    _ = random.randint(1,2)
+    if _ == 1:
+        chrgen = random.randint(97,122)
+        a = chr(chrgen)
+        print(a, end="")
+    if _ == 2:
+        numgen = random.randint(1,9)
+        print(numgen, end="")
+print("\n")
